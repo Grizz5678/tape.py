bcba7b7eb0f47bfd82151f44e7baeee78f6c2253
Grizz5678 <32799929+Grizz5678@users.noreply.github.com>
2019-11-24 17:14:31 -0500

    Add files via upload

---
+++ tape.py
@@ -0,0 +1,35 @@
+import sys, time
+for c in "Bonjour et bienvenue a la formation des formateurs pour le Raspberry Pi!":
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print
+time.sleep(1)
+for c in " Aujourd'hui nous allons  nous amuser a apprendre comment fonctionne le Raspberry Pi!":
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print
+time.sleep(1)
+for c in "   Nous aurons une phase d'exploration qui nous permettra de decouvrir":
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print
+time.sleep(1)
+for c in " cet ordinateur et plusieurs facons de l'utiliser et voir des exemples de projets!":
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print
+time.sleep(1)
+for c in " Vous serez egalement appeles a creer vos propres projets pour une prochaine   rencontre":
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print
+for c in " des services pedagogiques":
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.1)
+print
